4992bd11491502833103125ba8d1e9a5f57b31cc
Bini-Banal <zygbanal22@src.pshs.edu.ph>
2025-01-22 11:13:57 +0800

    Update script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+const Pi = 3.14;
+let R = 50;
+let C = 2 * Pi * R;
+let A = 4 * Pi * (R ** 2);
+let V = (4 * Pi * (R ** 3)) / 3;
+
+document.write('The radius of the circle is ', + R);
+document.write("<br><br>");
+document.write('The circumference is ', + C);
+document.write("<br><br>");
+document.write('The surface area is ', + A);
+document.write("<br><br>");
+document.write('The volume is ', + V);
